8bee20950a4b02f3cedd12b16ef443d4313523d1
De'Angelo Bennett <deangelo@beths-imac.lan>
2024-05-17 23:04:02 -0500

    now says Hello World, my name is De'Angelo

--- hello.js
+++ hello.js
@@ -4,9 +4,14 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log("Hello, World,", "My name is De'Angelo")  
+console.log("I am new here") 
+console.log("7" + "7")
+console.log('"Commit to learning new things"')
 /*  
+
+
+
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
